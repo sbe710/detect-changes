e02c03171a68c771c351f7020a7f1b99e41cd3a5
alep0518 <aleksandr.panasenko@netcracker.com>
2020-03-24 01:31:04 +0300

    Configured web-pack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -2,12 +2,32 @@ const {resolve} = require("path");
 const HTMLWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    resolve: {
-        extensions: [".ts", ".tsx", ".js", ".jsx"]
+    mode: "development",
+    entry: "./src/index.tsx",
+    output: {
+        path: resolve(__dirname, "dist"),
+        filename: "bundle.js"
     },
-    context: resolve(__dirname, "src"),
+    resolve: {
+        extensions: [".ts", ".tsx", ".js", ".jsx"],
+        alias: {
+            "@": resolve(__dirname, "src")
+        }
+    },
+    devServer: {
+        port: 4200,
+    },
     module: {
         rules: [
+            {
+                test: /\.tsx?$/,
+                use: ["babel-loader", "awesome-typescript-loader"]
+            },
+            {
+                test: /\.js$/,
+                use: ["babel-loader", "source-map-loader"],
+                exclude: /node_modules/
+            },
             {
                 test: /\.css$/,
                 use: [
@@ -16,25 +36,17 @@ module.exports = {
                 ]
             },
             {
-                test: /\.scss$/,
-                loaders: [
-                    "style-loader",
-                    { loader: "css-loader", options: { importLoaders: 1 } },
-                    "sass-loader"
-                ]
-            },
-            {
                 test: /\.(jpe?g|png|gif|svg)$/i,
-                loaders: [
+                use: [
                     "file-loader?hash=sha512&digest=hex&name=img/[hash].[ext]",
                     "image-webpack-loader?bypassOnDebug&optipng.optimizationLevel=7&gifsicle.interlaced=false"
-                ]
+                ],
             }
         ],
     },
     plugins: [
         new HTMLWebpackPlugin({
-            template: ""
+            template: "./src/index.html"
         })
     ]
 };
