d3aa123b36e4cab39863b29fb60b9afba9c3329f
alep0518 <aleksandr.panasenko@netcracker.com>
2020-03-26 02:53:03 +0300

    Update dev/prod webpack configs

---
+++ configs/webpack/production.js
@@ -0,0 +1,21 @@
+const {resolve} = require("path");
+const merge = require("webpack-merge");
+const CompressionPlugin = require("compression-webpack-plugin");
+
+const commonConfig = require("./common");
+
+module.exports = merge(commonConfig, {
+    mode: "production",
+    entry: "./index.tsx",
+    output: {
+        filename: "js/bundle.min.js",
+        path: resolve(__dirname, "../../dist"),
+        publicPath: "/"
+    },
+    devtool: "source-map",
+    plugins: [
+        new CompressionPlugin({
+            test: /\.(js|html|map)$/
+        })
+    ]
+});
